e5f7a44eac95f73b208b6373fd95dc5113dd16d5
chaquen <edgar.guzman21@gmail.com>
2020-11-23 12:56:41 -0500

    se agrega vista para modelo tienda

---
+++ src/views/CrearTienda.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+    <NavTienda></NavTienda>
+    <FormCrearTienda></FormCrearTienda>
+</div>
+</template>
+
+<script>
+import NavTienda from '../components/NavTienda.vue'
+import FormCrearTienda from '../components/FormCrearTienda.vue'
+
+export
+default {
+    name: 'About',
+    components: {
+        NavTienda,
+        FormCrearTienda
+    }
+}
+</script>
